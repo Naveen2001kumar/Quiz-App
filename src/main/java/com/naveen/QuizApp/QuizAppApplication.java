914b6b8802d7c7e4145c90d8fd412f830deb9c9d
Naveen2001kumar <2016079@saec.ac.in>
2023-09-21 20:35:39 +0530

    Annotation

--- src/main/java/com/naveen/QuizApp/QuizAppApplication.java
+++ src/main/java/com/naveen/QuizApp/QuizAppApplication.java
@@ -3,7 +3,8 @@ package com.naveen.QuizApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = "com.naveen.QuizApp")
 public class QuizAppApplication {
 
 	public static void main(String[] args) {
